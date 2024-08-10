a6ad7be953fe724005f76fe0919990593d221920
FelipeRodriguezMx <felipeRod0201@gmail.com>
2024-08-10 10:44:32 -0600

    implemented asyncronus progamming for pacakge

--- android/src/main/kotlin/com/hyterscan/hyterscan/ScanManagerHandler.kt
+++ android/src/main/kotlin/com/hyterscan/hyterscan/ScanManagerHandler.kt
@@ -6,114 +6,163 @@ import com.sim.scanner.ScannerManager.*
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import kotlinx.coroutines.*
+import kotlinx.coroutines.CompletableDeferred
 
-class ScanManagerHandler( context : Context) : MethodChannel.MethodCallHandler  {
+class ScanManagerHandler(context: Context) : MethodChannel.MethodCallHandler {
     private var scanCallback: ((Int, String?, String) -> Unit)? = null
     private var ctx: Context? = context
+    private val job = Job()
+    private val scope = CoroutineScope(Dispatchers.Main + job)
 
-    @OptIn(DelicateCoroutinesApi::class)
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
         when (call.method) {
-            "init"-> init()
-            "scan" -> scan(result)
-            "release" ->  GlobalScope.launch(Dispatchers.Main) {
+            "init" -> scope.launch {
+                init(result)
+            }
+            "scan" -> scope.launch {
+                scan(result)
+            }
+            "release" -> scope.launch {
                 release(result)
             }
             "hasInstances" -> result.success(checkInstance())
             else -> result.notImplemented()
         }
+
     }
 
-    @OptIn(DelicateCoroutinesApi::class)
-    private fun init(){
-           try {
-               GlobalScope.launch(Dispatchers.Main) {
-                   withContext(Dispatchers.Default) {
-                       init(ctx)
-                       listener()
-                   }
-               }
-           }catch (e: Exception){
-               e.message?.let { Log.d("Scanner", it) }
-           }
+    fun cancel() {
+        job.cancel()
     }
 
-    private fun  checkInstance(): Boolean {
-        if(getInstance() == null){
-            return false
+    private suspend fun init(result: MethodChannel.Result) {
+        val deferredResult = CompletableDeferred<Unit>()
+
+        scope.launch {
+            try {
+                withContext(Dispatchers.Default) {
+                    init(ctx)
+                    listener()
+                }
+                withContext(Dispatchers.Main) {
+                    deferredResult.complete(Unit)  // Completes successfully
+                    result.success("Initialization successful")
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    deferredResult.completeExceptionally(e)  // Completes with an exception
+                    result.error("Error", "Initialization failed", e.message)
+                }
+            }
         }
-        return true
+
+        // Await the deferred result (will be completed when the init operation is finished)
+        deferredResult.await()
     }
 
-    @OptIn(DelicateCoroutinesApi::class)
-    private fun listener(){
-        getInstance().addScannerManagerListener(object : ScannerManagerListener {
+
+    private fun checkInstance(): Boolean {
+        return getInstance() != null
+    }
+
+    private fun listener() {
+        getInstance()?.addScannerManagerListener(object : ScannerManagerListener {
             override fun Error(p0: Int, p1: String?) {
-                GlobalScope.launch(Dispatchers.Main) {
+                scope.launch {
                     scanCallback?.invoke(p0, p1, "ERROR")
                 }
             }
+
             override fun decodeResult(p0: Int, p1: String?) {
-                GlobalScope.launch(Dispatchers.Main) {
+                scope.launch {
                     scanCallback?.invoke(p0, p1, "SUCCESS")
                 }
             }
         })
     }
 
-    @OptIn(DelicateCoroutinesApi::class)
-    private suspend fun release(result: MethodChannel.Result){
-        if(getInstance() == null){
-            Log.d("Scanner", "ScanManager not registered")
-            result.error("Error", "No instance", "Instance is null")
+    private suspend fun release(result: MethodChannel.Result) {
+        val deferredResult = CompletableDeferred<Unit>()
+
+        val instance = getInstance()
+        if (instance == null) {
+            withContext(Dispatchers.Main) {
+                Log.d("Scanner", "ScanManager not registered")
+                deferredResult.completeExceptionally(Exception("No instance"))
+                result.error("Error", "No instance", "Instance is null")
+            }
             return
         }
+
         try {
             withContext(Dispatchers.Default) {
-                getInstance().ReleaseScanner()
+                instance.ReleaseScanner()
             }
             withContext(Dispatchers.Main) {
+                deferredResult.complete(Unit)  // Completes successfully with no result
                 result.success("Scanner released successfully")
             }
         } catch (e: Exception) {
-            Log.d("Scanner", "Failed to release scanner ${e.message}")
+            Log.d("Scanner", "Failed to release scanner: ${e.message}")
             withContext(Dispatchers.Main) {
+                deferredResult.completeExceptionally(e)  // Completes with an exception
                 result.error("Error", "Failed to release scanner", e.message)
             }
         }
+
+        // Await the deferred result (will be completed when the release operation is finished)
+        deferredResult.await()
     }
 
-    @OptIn(DelicateCoroutinesApi::class)
-    private fun scan(result: MethodChannel.Result) {
-        GlobalScope.launch(Dispatchers.IO) {
-            val instance = getInstance()
-            if (instance == null) {
-                withContext(Dispatchers.Main) {
-                    Log.d("Scanner", "ScanManager not registered")
-                    result.error("Error", "No instance", "Instance is null")
-                }
-                return@launch
+    private suspend fun scan(result: MethodChannel.Result) {
+        val deferredResult = CompletableDeferred<String?>()
+
+        val instance = getInstance()
+        if (instance == null) {
+            withContext(Dispatchers.Main) {
+                Log.d("Scanner", "ScanManager not registered")
+                deferredResult.completeExceptionally(Exception("No instance"))
+                result.error("Error", "No instance", "Instance is null")
             }
+            return
+        }
 
-            val openScannerStatus = instance.OpenScanner()
-            if (openScannerStatus == BCR_ERROR) {
-                withContext(Dispatchers.Main) {
-                    result.error("Error", "Scanner open error", openScannerStatus)
-                }
-                return@launch
+        val initScanner = withContext(Dispatchers.Default) { instance.initScanner() }
+        if (initScanner == BCR_ERROR) {
+            withContext(Dispatchers.Main) {
+                deferredResult.completeExceptionally(Exception("Scanner init error"))
+                result.error("Error", "Scanner init error", initScanner)
             }
+            return
+        }
 
-            GlobalScope.launch(Dispatchers.Main) {
-                scanCallback = { errorCode, scannedValue, status ->
+        val openScannerStatus = withContext(Dispatchers.Default) { instance.OpenScanner() }
+        if (openScannerStatus == BCR_ERROR) {
+            withContext(Dispatchers.Main) {
+                deferredResult.completeExceptionally(Exception("Scanner open error"))
+                result.error("Error", "Scanner open error", openScannerStatus)
+            }
+            return
+        }
 
-                    if (status == "SUCCESS") {
-                        result.success(scannedValue)
-                    } else {
-                        result.error("Error", "Scanner listener", errorCode)
-                    }
-                }
+        // Set up the callback to complete the deferred result
+        scanCallback = { errorCode, scannedValue, status ->
+            if (status == "SUCCESS") {
+                deferredResult.complete(scannedValue)
+                result.success(scannedValue)
+            } else {
+                val error = Exception("Scanner listener error: $errorCode")
+                deferredResult.completeExceptionally(error)
+                result.error("Error", "Scanner listener", errorCode)
             }
         }
+
+        // Wait for the result (will be completed by the callback)
+        deferredResult.await()
     }
+
+
+
 }
+
 
