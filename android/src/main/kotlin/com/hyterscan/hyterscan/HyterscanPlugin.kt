bcaecbd39a642c23eabd9df2b1b6916556420ebf
FelipeRodriguezMX <67709343+FelipeRodriguezMX@users.noreply.github.com>
2024-12-30 13:50:33 -0600

    Feat/async handleing (#7)
    * implemented asyncronus progamming for pacakge
    
    * optimizied plugin for releasing memory

--- android/src/main/kotlin/com/hyterscan/hyterscan/HyterscanPlugin.kt
+++ android/src/main/kotlin/com/hyterscan/hyterscan/HyterscanPlugin.kt
@@ -6,32 +6,35 @@ import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
 
-class HyterscanPlugin: FlutterPlugin, ActivityAware {
-  private var method : MethodChannel?= null
+class HyterscanPlugin : FlutterPlugin, ActivityAware {
+    private var methodChannel: MethodChannel? = null
+    private var scanManagerHandler: ScanManagerHandler? = null
 
-  override fun onAttachedToEngine(flutter: FlutterPlugin.FlutterPluginBinding) {
-    method = MethodChannel(flutter.binaryMessenger, "scanner")
-  }
+    override fun onAttachedToEngine(flutterBinding: FlutterPlugin.FlutterPluginBinding) {
+        methodChannel = MethodChannel(flutterBinding.binaryMessenger, "scanner")
+    }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        methodChannel?.setMethodCallHandler(null)
+        methodChannel = null
+    }
 
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    val handler = ScanManagerHandler(binding.activity.applicationContext)
-    method?.setMethodCallHandler(handler)
-  }
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        scanManagerHandler = ScanManagerHandler(binding.activity.applicationContext)
+        methodChannel?.setMethodCallHandler(scanManagerHandler)
+    }
 
-  override fun onDetachedFromActivityForConfigChanges() {
-    onDetachedFromActivity()
-  }
+    override fun onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity()
+    }
 
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    onAttachedToActivity(binding)
-  }
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        onAttachedToActivity(binding)
+    }
 
-  override fun onDetachedFromActivity() {
-    method?.setMethodCallHandler(null)
-    method = null
-  }
+    override fun onDetachedFromActivity() {
+        scanManagerHandler?.cancel()
+        methodChannel?.setMethodCallHandler(null)
+        scanManagerHandler = null
+    }
 }
-
-
